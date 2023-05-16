23310a607ffc1c14db8b9f058ab4f5dbfc18ba0c
CameronPike24 <115454082+CameronPike24@users.noreply.github.com>
2023-05-16 15:48:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,99 +4,97 @@ from kivy.properties import NumericProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy_garden.graph import Graph, LinePlot
 import numpy as np
-from kivy.clock import Clock
-
-#from tools import AudioPlayer
-#from kivy.core.audio import SoundLoader
+#from android_permissions import AndroidPermissions
+from android.permissions import request_permissions,Permission,check_permission
 
 
-import time
-import wave
-from audiostream import get_input
-
-frames = []
-
-'''
-def mic_callback(buf):
-    print('got', len(buf))
-    frames.append(buf)
-
-# get the default audio input (mic on most cases)
+from tools import AudioPlayer
 
 
-mic = get_input(callback=mic_callback)
-mic.start()
+class MainApp(App):
 
-time.sleep(5)
-
-mic.stop()
-
-wf = wave.open("test.wav", 'wb')
-wf.setnchannels(mic.channels)
-wf.setsampwidth(2)
-wf.setframerate(mic.rate)
-wf.writeframes(b''.join(frames))
-wf.close()
-
-
-
-
-def mic_callback(buf):
-    print('got', len(buf))
-    frames.append(buf)
-    print('size of frames: ' + len(frames))
-
-def bcallback(instance):
-    mic = get_input(callback=mic_callback, source='mic')
-    mic.start()
-    #mic.poll()
-    time.sleep(5)
-    mic.stop()
-
-class MyApp(App):
     def build(self):
-        btn1 = Button(text='Audio Record')
-        btn1.bind(on_press=bcallback)
-        return btn1
+        request_permissions([Permission.INTERNET, Permission.RECORD_AUDIO])
+        
+        #request_permissions([Permission.INTERNET, Permission.RECORD_AUDIO,
+        #                    Permission.READ_EXTERNAL_STORAGE,Permission.WRITE_EXTERNAL_STORAGE])
+        self.app = MainGrid()       
 
-#if name == 'main':
-if __name__=='__main__':
-    MyApp().run()
-'''    
-    
-def __init__(self, **kw):
-    super().__init__(**kw)
-    self.samples_per_second = 60 # variables which stores the number of audio samples recorded per second
-    self.audioData = [] # creates a list to store the audio bytes recorded
-    #import sys
-    #importlib.reload(sys.modules['audiostream']) # reloads the audiostream module - thought this might solve the problem; it doesn't!!
-    self.mic = get_input(callback=self.micCallback, rate=8000, source='default', buffersize=2048) # initialises the method get_input from the module 'audiostream' with the properties required to ensure the audio is recorded correctly
+        return self.app
+        
+    #def on_start(self, *args):
+        #self.dont_gc = AndroidPermissions(self.start_app)
 
-def micCallback(self, buffer):
-    # method which is called by the method 'get_input' to store recorded audio data (each buffer of audio samples)
-    self.audioData.append(buffer) # appends each buffer (chunk of audio data) to variable 'self.audioData'
-    print('size of frames: ' + len(self.audioData))
+    #def start_app(self):
+        #self.dont_gc = None        
+        
+        
+        
+        
 
-def start(self):
-    # method which begins the process of recording the audio data
-    self.mic.start() # starts the method 'self.mic' recording audio data
-    Clock.schedule_interval(self.readChunk, 1 / self.samples_per_second) # calls the method 'self.readChunk' to read and store each audio buffer (2048 samples) 60 times per second
+    def init_thread(self):
+        self.playback_thread = threading.Thread(target=self.app.player.run)
+        # daemon threads don't wait for main thread
+        self.playback_thread.setDaemon(True)
+        self.playback_thread.start()
+        print("Playback Thread", self.playback_thread.native_id, "started")
+        print("Main Thread", threading.main_thread().native_id)
 
-def readChunk(self, sampleRate):
-    # method which coordinates the reading and storing of the bytes from each buffer of audio data (which is a chunk of 2048 samples)
-    self.mic.poll()  # calls 'get_input(callback=self.mic_callback, source='mic', buffersize=2048)' to read the byte content. This byte content is then dispatched to the callback method 'self.micCallback'
+    def exit_thread(self):
+        self.playback_thread.join()
+        print("Playback Thread", self.playback_thread.native_id, "stopped")
 
-def stop(self):
-    # method which terminates and saves the audio recording when the recording has been successful
-    Clock.unschedule(self.readChunk) # un-schedules the Clock's rythmic execution of the 'self.readChunk' callback
-    self.mic.stop() # stops recording audio
-    return self.audioData    
-    
-#messageFile_voice = SoundLoader.load("recording2.wav")
-#messageFile_voice.play()    
-    
-if __name__=='__main__':
-    MyApp().run()    
-    
-    
-    
+class MainGrid(BoxLayout):
+
+    zoom = NumericProperty(1)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.samples = 512
+        self.zoom = 1
+        
+        self.graph = Graph(y_ticks_major=0.5,
+                           x_ticks_major=64,
+                           border_color=[0, 1, 1, 1],
+                           tick_color=[0, 1, 1, 0.7],
+                           x_grid=True, y_grid=True,
+                           xmin=0, xmax=self.samples,
+                           ymin=-1.0, ymax=1.0,
+                           draw_border=False,
+                           x_grid_label=True, y_grid_label=False)
+        # Mono, Sampling Rate, Chunksize
+        self.player = AudioPlayer(1, 44100, self.samples)
+        self.ids.modulation.add_widget(self.graph)
+        self.plot_x = np.linspace(0, 1, self.samples)
+        self.plot_y = np.zeros(self.samples)
+        self.plot = LinePlot(color=[1, 1, 0, 1], line_width=1.5)
+        self.old_freq = 0
+        self.freq = 0
+        # adds plot to the graph widget
+        self.graph.add_plot(self.plot)
+        self.update_plot(1)
+
+    def update_plot(self, freq):
+        self.plot_y = np.sin(2*np.pi*freq*self.plot_x)
+        # draws plot
+        self.plot.points = [(x, self.plot_y[x]) for x in range(self.samples)]
+
+    def update_zoom(self, value):
+        if value == '+' and self.zoom < 8:
+            self.zoom *= 2
+            self.graph.x_ticks_major /= 2
+        elif value == '-' and self.zoom > 1:
+            self.zoom /= 2
+            self.graph.x_ticks_major *= 2
+
+    def play_result(self):
+        if self.ids.play.state == 'down':
+            self.ids.play.text = '[b]STOP[/b]'            
+            App.get_running_app().init_thread()
+        else:
+            self.ids.play.text = '[b]PLAY[/b]'
+            self.player.stop()
+            App.get_running_app().exit_thread()
+
+
+MainApp().run()
